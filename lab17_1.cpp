720fb747ed2b6e07de469ee66ac34e32564a29ec
Bigloso <wayside.big@gmail.com>
2020-03-07 23:38:32 +0700

    last final

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std ;
 
-string toopper_for_str(string x){
+string toupper_for_str(string x){
     string y = x ;
     for(int i = 0;i < x.size();i++){
         y[i] = toupper(x[i]);
@@ -43,22 +43,28 @@ int main(){
     for(int i=0; ; i++){
         cout << "Please input your command :" ;
         getline(cin,asd) ;
-        if(asd == "exit") break ;
-            else if(asd.substr(0,6) == "grade " ||asd.substr(0,6) == "GRADE " ){
-                asd = toopper_for_str(asd) ;
+        asd = toupper_for_str(asd);
+        if(asd == "EXIT") break ;
+            else if(asd.substr(0,6) == "GRADE " ){
                 for(int j=0;j<26;j++){
                     if(asd.substr(6,1) == grade[j] ){
                         cout << nam[j] << endl ;
                     }
+                    if(asd.substr(6,1) != "A" & asd.substr(6,1) != "B" & asd.substr(6,1) != "C" & asd.substr(6,1) != "D" & asd.substr(6,1) != "F" ){
+                       cout << "Cannot Found." << endl ;
+                      break ;
+                    }
+                   
                 }
             }
-            else if(asd.substr(0,5) == "name " ||asd.substr(0,5) == "NAME "){
-                asd = toopper_for_str(asd);
+            else if(asd.substr(0,5) == "NAME "){
                 for(int k=0;k<26;k++){
-                    qwe = toopper_for_str(nam[k]);
+                    qwe = toupper_for_str(nam[k]);
                     if(asd.substr(5,asd.size()-5) == qwe){
                         cout << nam[k] <<"'s "<< "grade = " << grade[k] << endl;
+                        break ;
                     }
+                    if(k == 25 ) cout << "Cannot found." << endl ;
                 }
             }
                 else cout << "Invalid command" << endl;
